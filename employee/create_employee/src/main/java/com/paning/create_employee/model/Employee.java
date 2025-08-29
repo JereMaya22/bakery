a5415e3c92a8678726c25c82049e7277d8005eaa
JereMaya22 <jeremy.rivera409@gmail.com>
2025-08-29 11:31:04 -0600

    started with micro service of employee

---
+++ employee/create_employee/src/main/java/com/paning/create_employee/model/Employee.java
@@ -0,0 +1,15 @@
+package com.paning.create_employee.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private Long id;
+    private String name;
+}
---
+++ employee/create_employee/src/test/java/com/paning/create_employee/CreateEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paning.create_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
