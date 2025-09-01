6ff69226a10202b00d422da8b36bc5b47e817aee
JereMaya22 <jeremy.rivera409@gmail.com>
2025-09-01 13:56:39 -0600

    strated with getallemployee

---
+++ employee/getallemployee/src/main/java/com/paning/getallemployee/modeldto/getEmployeeDTO.java
@@ -0,0 +1,12 @@
+package com.paning.getallemployee.modeldto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class getEmployeeDTO {
+    private String name;
+}
